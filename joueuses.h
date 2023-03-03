5b9cd1e0bab986eff897b8b81212ad4c3c7921da
Theo TAILLEFUMIER <theo.taillefumier@pc237-02.pedago.ensiie.fr>
2023-03-03 17:46:04 +0100

    Tache A.2 création des fonctions

---
+++ joueuses.h
@@ -0,0 +1,27 @@
+#ifdef
+#include personnages.h
+#include cartes.h
+typedef struct joueuse
+{
+    int capital;
+    membre
+};
+
+
+joueuse creation_jouese() {}
+
+void free_joueuse(joueuse j) {}
+
+joueuse tour_joueuse() {}
+
+membre* membre_ecole(joueuse j) {}
+
+int nb_membre_ecole(joueuse j) {}
+
+int tous_manges(joueuse j) {/* return (nb_membre_ecole == 0) */}
+
+void reinitialise_capital(joueuse j) {}
+
+void utilise_capital(joueuse j, int capital)
+
+#ifndef
