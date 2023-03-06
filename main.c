8192ab779266e8c2d02e231b80cd02ef1f13187a
Asmo <asmoderu94@gmail.com>
2023-03-06 23:24:23 +0100

    Tache A1 - Début2 main.c

--- main.c
+++ main.c
@@ -17,7 +17,7 @@ int main() {
 
     joueuse joueuse_1 = creation_joueuse();
     joueuse joueuse_2 = creation_joueuse();
-    joueuse *joueuse_courant;
+    joueuse *joueuse_courante = &joueuse_1;
     fprintf(stderr, "Etape 1: creation des joueuses succes.\n");
 
 
@@ -27,9 +27,16 @@ int main() {
      * la partie se termine lorsqu'une des deux joueuses n'a plus de personnages.
      * 
      */
+
+    joueuse *liste_joueuses[] = { &joueuse_1, &joueuse_2 };
+    
     while(!tous_manges(joueuse_1) || !tous_manges(joueuse_2)) {
 
-        for()
+        
+
+        for (int i = 0; i < 2; i += 1) {
+            
+        }
 
     }
 
