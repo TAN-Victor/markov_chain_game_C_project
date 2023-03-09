f76a4040b5494bf6be0cf5d7f1af40ece11573ec
Victor Tan <victor285@gmail.com>
2023-03-09 10:51:37 +0100

    Tache A1 - Ajout début du main.c, attente A4

--- main.c
+++ main.c
@@ -33,8 +33,46 @@ int main() {
     while(!tous_manges(joueuse_1) || !tous_manges(joueuse_2)) {
 
         for (int i = 0; i < 2; i += 1) {
-            
-        }
+
+            /**
+             * @brief Information du jeu
+             * 
+             */
+            printf("C'est le tour du joueur n° %d, qui a un capital de %d points. \n", i, liste_joueuses[i]->capital);
+
+            /**
+             * @brief Choix d'une action
+             * 
+             */
+
+            int choix = -1;
+            printf("Choississez votre action à effectuer: \n \t * 1 : Utiliser du capital \n \t * 2 : Utiliser une carte \n \t * 3 : Directement se déplacer\n");
+
+            while (choix < 0) {
+                if (scanf("%d", &choix) != 1) {
+                    while (getchar() != '\n') { // Permet de réinitialiser le choix
+                        continue;
+                    }
+                }
+                switch (choix) {
+                    case 1:
+                        printf("Vous avez choisi le choix n° %d.\n", choix);
+                        //utiliser_capital();
+                        break;
+                    case 2:
+                        printf("Vous avez choisi le choix n° %d.\n", choix);
+                        //utiliser_carte();
+                        break;
+                    case 3:
+                        printf("Vous avez choisi le choix n° %d.\n", choix);
+                        break;
+                    default:
+                        printf("Désolé, veuillez entrer un choix correct.\n");
+                        choix = -1;
+                        break;
+                }
+            }
+            //moveTo(&monstre, nextZone);
 
     }
 
