732885b719c78191f2f0c14015b5c8b2f550cdd2
Adam OUZEGDOUH <adam.ouzegdouh@ensiie.fr>
2023-03-09 16:41:29 +0000

    Tâche A.4 - le type carte avec une idée d'implémentation pour la suite en commentaire du fichier

---
+++ cartes.h
@@ -0,0 +1,22 @@
+/**
+ * \file cartes.h
+ * Ce fichier décrit le type \b carte qui sera composé d'un \b nom et d'une \b description tout deux de type \b char* .
+ * De plus, par la suite un tableau statique de taille 20 qui contiendra les 20 cartes du jeu sera implémenté dans le \b main.c au début,
+ * ainsi que 2 autres tableaux de cartes de taille 5 contenant les cartes tirées qui seront la main de chaque joueur.
+*/
+
+struct carte{
+    char* nom;
+    char* description;
+};
+typedef struct carte carte;
+
+
+
+
+
+
+
+
+
+
