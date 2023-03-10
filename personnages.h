a41c5325149de8b2028832bbf31dbdbceeea01a1
Noah KWA MOUTOME <noah.kwamoutome@pc130-18.pedago.ensiie.fr>
2023-03-10 12:07:57 +0100

    Tâche A.3 : 2e commit et finition zones.h et personnages.h

--- personnages.h
+++ personnages.h
@@ -1,54 +1,70 @@
-/*
-@requires:
-@assigns:
-@ensures:
-*/
+/**
+ * @file personnages.h
+ * @author Noah Kwa Moutome
+ * @brief interface des personnages
+ * @date 2023-03-10
+ * 
+ * @necessities zones.h 
+ * 
+ */
+#ifndef PERSONNAGES_H
+#define PERSONNAGES_H
 
 #include "zones.h"
 
-struct personnage {
+struct _personnage {
     int type; /* 0 pour monstre et 1 ou 2 pour membre d'école*/
-    int status; /* 0 si mort, 1 si en vie */
-    zone current_zone; /* zone courrante sur laquelle se trouve le personnage*/
+    int statut; /* 0 si mort, 1 si en vie */
+    zone zone_courante; /* zone courante sur laquelle se trouve le personnage*/
+    int nb_de_pas_restant; /* nombre de pas restant, initialisé à 1 à chaque tour */
 };
-typedef struct personnage personnage;
+typedef struct _personnage personnage;
 
-/*
-@requires: nothing
-@assigns: nothing
-@ensures: renvoie un pointeur vers un personnages de type:
-        - monstre si n = 0
-        - membre d'école de joueuse 1 si n = 1
-        - membre d'école de jouese 2 si n = 2,
-    le place sur la zone z passée en argument
-    et initialise son statut sur vivant (ie 1)
-*/
-personnage* newPersonnage(int n, zone z);
+/**
+ * @brief renvoie un pointeur vers un personnages de type:
+            - monstre si n = 0
+            - membre d'école de joueuse 1 si n = 1
+            - membre d'école de jouese 2 si n = 2,
+          le place sur la zone z passée en argument
+          et initialise son statut sur vivant (ie 1)
+ * 
+ * @param n le type de personnage
+ * @param z le numéro de la zone
+ * @return personnage* un pointeur vers le personnage crée 
+ */
+personnage* nouveauPersonnage(int n, zone z);
 
-/*
-@requires: une adresse vers un personnage valide pp
-@assigns: la valeur de la case mémoire pointée par le pointeur passé en argument
-@ensures: libère l'espace occupé par la case mémoire pointée par pp 
-*/
-void freePersonnage(personnage* pp);
+/**
+ * @brief libère l'espace occupé par la case mémoire pointée par pp 
+ * 
+ * @param pp pointeur vers la case mémoire occupée par le personnage que l'on souhaite libérer 
+ */
+void liberePersonnage(personnage* pp);
 
-/*
-@requires: une adresse vers un personnage valide pp, une zone existante z
-@assigns: la case mémoire pointée par pp
-@ensures: le personnage pointé par pp se trouve maintenant sur la zone z
-*/
-void moveTo(personnage* pp, zone z);
 
-/*
-@requires: une adresse vers un personnage valide pp
-@assigns: nothing
-@ensures: renvoie la zone dans laquelle se trouve le personnage pointé par pp
-*/
-zone getZone(personnage* pp);
+/**
+ * @brief déplace le personnage pointé sur la zone z
+ * 
+ * @param pp est un pointeur vers la case mémoire du personnage qui se déplace
+ * @param z est la zone d'arrivée
+ */
+void deplacer(personnage* pp, zone z);
 
-/*
-@requires: une adresse vers un personnage valide pp
-@assigns: la case mémoire pointée par pp
-@ensures: met à jour le statut du personnage pointé par pp
-*/
-void gotEaten(personnage* pp);
+
+/**
+ * @brief renvoie la zone dans laquelle se trouve le personnage
+ * 
+ * @param pp un personnage dont on souhaite obtenir la zone
+ * @return la zone du personnage
+ */
+zone zonePersonnage(personnage p);
+
+
+/**
+ * @brief met à jour le statut du personnage pointé par pp
+ * 
+ * @param pp un pointeur vers le personnage qui a été mangé
+ */
+void estMange(personnage* pp);
+
+#endif
--- zones.h
+++ zones.h
@@ -1,43 +1,70 @@
-struct zones{
-    int tab_zones[10]; /* la case i représente la zone i */
-    float tab_proba[10]; /* la case i contient la proba d'aller sur la case i */
+/**
+ * @file zones.h
+ * @author Noah Kwa Moutome
+ * @brief interface des zones
+ * @date 2023-03-10
+ * 
+ * @necessities 
+ * 
+ */
+#ifndef ZONE_H
+#define ZONE_H
+
+struct _zone {
+    int numero; /* numéro de la zone */
+    float proba; /* proba de se déplacer vers cette zone */
 };
-typedef struct zones zones;
+typedef struct _zone zone;
 
-struct zone;
-typedef struct zone zone;
+struct _zones{
+    /* 11 zones car la carte "Lucienne Pacave" ajoute une 11e zone au jeu*/
+    zone tab_zones[11]; /* la case i représente la zone i */
+};
+typedef struct _zones zones;
 
-/*
-@requires: nothing
-@assigns: nothing
-@ensures: renvoie un pointeur vers l'ensemble des zones du jeu
-*/
-zones* newZones();
 
-/*
-@requires: une adresse vers un ensemble de zone valide pz
-@assigns: la case mémoire pointée par pz
-@ensures: libère toutes les zones du jeu
-*/
-void freeZones(zones* pz);
+/**
+ * @brief créer les 11 zones du jeu, avec une proba de 0 pour la 11e
+ *        et 0.1 pour chacune des autres
+ * 
+ * @return renvoie un pointeur vers l'ensemble des zones du jeu 
+ */
+zones* nouvellesZones();
 
-/*
-@requires: un entier n compris entre 0 et 9
-@assigns: nothing
-@ensures: renvoie un pointeur vers la zone numéro n
-*/
-zone* getZone(int n);
 
-/*
-@requires: une adresse vers un ensemble de zones valide pz
-@assigns: les cases mémoires de tab_proba de la case pointée par pz
-@ensures: met à jour le tableau en fonction de l'argument action passé en paramètre (voir joueuse.h)
-*/
-void applyDecisions(zones* pz /* insérer une action en paramètre ici*/);
+/**
+ * @brief libère toutes les zones du jeu
+ * 
+ * @param pz une adresse vers un ensemble de zones valide
+ */
+void libereZones(zones* pz);
 
-/*
-@requires: un entier n compris entre 0 et 9
-@assigns: nothing
-@ensures: renvoie un pointeur vers la zone successeur de la zone numéro n en fonction des proba présentent dans ens_zone
-*/
-zone* nextZone(zones ens_zones, int n);
+
+/**
+ * @brief renvoie un pointeur vers la zone dont le numéro est n
+ * 
+ * @param n numéro de la zone
+ * @return zone* pointeur vers la zone de numéro n
+ */
+zone* trouveZone(int n);
+
+
+/**
+ * @brief modifie la probabilité de la zone en fonction du capital investi
+ *        proba est augmenté/diminué de 0.1 par capital investi
+ * @param n numéro de la zone
+ * @param capital capital investi
+ * @param action indique si on augmente (action = 1) ou retire (action = 0) de la probabilité
+ */
+void modifierZone(int n, int capital, int action);
+
+
+/**
+ * @brief renvoie la prochaine zone
+ * 
+ * @param ens_zones 
+ * @return la prochaine zone
+ */
+zone prochaineZone(zones ens_zones);
+
+#endif
