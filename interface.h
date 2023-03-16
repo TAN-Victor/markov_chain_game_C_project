e1c907976a7d3563a610e3166a18ec4febea0ba4
Adam OUZEGDOUH <adam.ouzegdouh@ensiie.fr>
2023-03-16 13:33:17 +0000

    Tâche A.4 - Modification en fonction des suggestions de Victor.

--- interface.h
+++ interface.h
@@ -2,21 +2,22 @@
  * \file interface.h
  * Ce fichier décrit un ensemble de fonctions utile à une interface optimale.
  * Il contient les 5 fonctions suivantes :
- * - \b afficher_toute_info(joueuse[],zones,personnage[]) qui permet d'afficher dans la console toutes les informations du jeu: les zones du jeu, les personnages qui sont dessus, la joueuse dont c'est le tour, le capital des joueuses, ...
+ * - \b afficher_toute_info(joueuse,joueuse,zones,personnage[]) qui permet d'afficher dans la console toutes les informations du jeu: les zones du jeu, les personnages qui sont dessus, la joueuse dont c'est le tour, le capital des joueuses, ...
  * - \b demander_capital(joueuse) pour demander à une joueuse combien de capital elle veut utiliser (elle peut répondre ne pas vouloir en utiliser)
  * - \b demander_carte(joueuse,carte[]) pour demander à une joueuse si elle veut jouer une carte et si oui, laquelle.
- * - \b afficher_fin_du_jeu(joueuse) qui permet d'afficher dans la console un message quand le jeu est fini.
- * - \b afficher_info(personnage,joueuse) pour afficher des messages génériques (par exemple si un membre a été mangé).
+ * - \b demander_zones(joueuse) pour demander à une joueuse sur quel zone il faudrait augmenter la probabilité et sur laquelle il faudrait la baisser
+ * - \b message_fin_du_jeu(joueuse) qui permet d'afficher dans la console un message quand le jeu est fini.
+ * - \b messsage_generique(int,joueuse,int) pour afficher des messages génériques (par exemple si un membre a été mangé).
 */
 
 /**
  * \brief Affiche dans la console toutes les informations du jeu: les zones du jeu, les personnages qui sont dessus, la joueuse dont c'est le tour, le capital des joueuses, ...
- * \param joueuse[] est un tableau contenant 2 éléments de type joueuse afin de récupérer les informations leur concernant.
+ * \param joueuse,joueuse sont les 2 joueuses afin de récupérer les informations leurs concernants.
  * \param zones afin de récupérer les informations sur toute les zones du jeu.
  * \param personnage[] est un tableau de tout les personnages présent sur le plateau (Pour avoir accès aux informations leurs concernants)
  * \return rien.
 */
-void afficher_toute_info(joueuse[],zones,personnage[]);
+void afficher_toute_info(joueuse,joueuse,zones,personnage[]);
 
 /**
  * \brief Demande à une joueuse combien de capital elle souhaite utiliser (elle peut répondre ne pas vouloir en utiliser)
@@ -27,24 +28,30 @@ int demander_capital(joueuse);
 
 /**
  * \brief Demande à une joueuse si elle veut jouer une carte et si oui, laquelle.
- * \param joueuse afin de savoir à quelle joueuse demander.
- * \param carte[] qui correspond à la main de la joueuse.
+ * \param joueuse afin de savoir à quelle joueuse demander (les cartes associées sont déjà dans les fonctions de joueuses.h)
  * \return le nom de la carte qu'elle souhaite utiliser, NULL sinon.
 */
-char* demander_carte(joueuse,carte[]);
+char* demander_carte(joueuse);
 
 /**
- * \brief Affiche dans la console un message quand le jeu est fini.
- * \param joueuse afin d'avoir l'information si tout les membres de l'école de la joueuse ont été mangés.
- * \return rien.
+ * \brief Demande à une joueuse où elle veut augmenter la probabilité et une autre où elle doit diminuer, elle peut répondre NULL si elle veut rien modifier
+ * \param joueuse afin de savoir à quelle joueuse demander (les zones associes sont déjà dans les fonctions de joueuses.h)
+ * \return un tableau contenant 2 int représentant le numéro des zones où la probabilité est à modifier(le premier élément sera celle à augmenter et le deuxième celle à diminuer), NULL sinon.
 */
-void afficher_fin_du_jeu(joueuse);
+int* demander_zones(joueuse);
 
 /**
- * \brief Affiche des messages génériques (par exemple si un membre a été mangé).
- * \param joueuse afin d'avoir des informations concernant le joueur.
- * \param personnage afin d'avoir des informations concernant un des personnages du jeu.
- * \return rien.
+ * \brief Affiche dans la console un message quand le jeu est fini
+ * \param joueuse,joueuse afin d'avoir les informations si tout les membres de l'école d'une joueuse ont été mangés
+ * \return rien
 */
-void afficher_info(joueuse,personnage);
+void message_fin_du_jeu(joueuse,joueuse);
+
+/**
+* \brief Affiche des messages génériques.
+* \param int Numéro du message à afficher.
+* \param joueuse Joueuse concernéé par le message.
+* \param int Index du personnage de la joueuse ou index du numéro de carte utilisé, ... m=0 lorsqu'il n'est pas utile.
+*/
+void message_generique(int,joueuse,int);
 
