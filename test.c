e7826f100b31805a5082ba963c4a3ffdc89fea8c
Theo TAILLEFUMIER <theo.taillefumier@pc237-08.pedago.ensiie.fr>
2023-03-31 17:29:42 +0200

    Tâche B.2

---
+++ test.c
@@ -0,0 +1,20 @@
+/**
+ * @file test.c
+ * @author Théo TAILLEFUMIER 
+ * @brief 
+ * @necessities 
+ * @date 2023-03-31
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int test_init_joueuses(joueuses_t joueuses)
+{
+    if(joueuses == NULL)
+    {
+        return 0;
+    }
+    return 1;
+}
