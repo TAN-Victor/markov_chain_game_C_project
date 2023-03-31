b31a22185f506e195262bf4f81703a4084990b0d
Adam OUZEGDOUH <adam.ouzegdouh@ensiie.fr>
2023-03-31 15:44:16 +0000

    Tâche B7 - Implémentation des fonctions pouvoir_carte pour chaque carte

--- src/cartes.c
+++ src/cartes.c
@@ -1,3 +1,35 @@
 #include "../headers/cartes.h"
 #include "../headers/personnages.h"
 #include "../headers/cartes.h"
+#include "../headers/joueuses.h"
+#include "../headers/zones.h"
+#include "../headers/interface.h"
+
+ /**
+ * \brief Créer une carte vide
+ * \return Retourne une carte vide
+*/
+carte creer_carte(){
+    carte c=(carte)malloc(sizeof(carte));
+    return c;
+}
+
+/**
+ * @brief renvoie le nom d'une carte
+ * 
+ * @param c 
+ * @return char* le nom de la carte
+ */
+char* getNom(carte c){
+    return c->nom;
+}
+
+/**
+ * @brief renvoie la description de la carte
+ * 
+ * @param c 
+ * @return char* la description
+ */
+char* getDescription(carte c){
+    return c->description;
+}
