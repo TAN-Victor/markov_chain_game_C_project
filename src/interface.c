f52dc5b26e1405ff7a3cd7ddc355064f779ee6f4
Victor Tan <victor285@gmail.com>
2023-04-14 15:24:28 +0200

    Tache B8 - Premiers codages de toutes les fonctions, il reste à modifier les accès aux attributs en utilisant les getters

--- src/interface.c
+++ src/interface.c
@@ -114,7 +114,7 @@ int demander_capital(joueuse j1) {
 */
 carte demander_carte(joueuse j1) {
     int choix = 0;
-    int taille_main_cartes = j1.main_du_joueur->nb_cartes;
+    int taille_main_cartes = j1.main_du_joueur->nb_cartes; //TODO
     printf("Vous avez les cartes suivantes: \n");
     for (int i = 0; i < taille_main_cartes; i += 1) {
         printf("%s  ", getName(lecture_cartes(getMains(j1), i)));
@@ -141,21 +141,85 @@ carte demander_carte(joueuse j1) {
  * \param zones afin de savoir sur quelle zone intéragir (un paramamètre joueuse n'est pas utile car dans le main on sait déjà sur quelle joueuse demander)
  * \return un tableau contenant 3 int représentant le numéro de la zone de départ et 2 zones où la probabilité est à modifier(le premier sera celle à augmenter et le deuxième celle à diminuer), NULL sinon
 */
-int* demander_zones(zones);
+int* demander_zones(zones liste_zones) {
+    int choix_a_augmenter = -1;
+    int choix_a_diminuer = -1;
+    int choix_depart = -1;
+    int nombre_de_zones = NB_DE_ZONES;
+    printf("Choissez une zone de départ entre 1 et %d: \n", nombre_de_zones);
+    scanf("%d", &choix_depart);
+    if (choix_depart <= 0 || choix_depart > nombre_de_zones) {
+        fprintf(stderr, "Attention, vous n'avez pas entré une zone correcte.\n");
+        return null;
+    }
+    printf("Choisissez une zone dont vous souhaitez AUGMENTER la probabilité entre 1 et %d: \n", nombre_de_zones);
+    scanf("%d", &choix_a_augmenter);
+
+    if (choix_a_augmenter <= 0 || choix_a_augmenter > nombre_de_zones) {
+        fprintf(stderr, "Attention, vous n'avez pas entré une zone correcte.\n");
+        return null;
+    }
+    else {
+        printf("Vous avez choisi la zone %d; \n Choisissez une zone dont vous souhaitez DIMINUER la probabilité entre 1 et %d (%d exclu): \n", choix_a_augmenter, nombre_de_zones, choix_a_diminuer);
+        if (choix_a_diminuer <= 0 || choix_a_diminuer > nombre_de_zones) {
+            fprintf(stderr, "Attention, vous n'avez pas entré une zone correcte.\n");
+            return null;
+        }
+        else {
+            printf("Vous avez choisi la zone %d. \n", choix_a_diminuer);
+            int *triplet = [choix_depart, choix_a_augmenter, choix_a_diminuer];
+            return triplet;
+        }
+    }
+}
 
 /**
  * \brief Affiche dans la console un message quand le jeu est fini
  * \param joueuse,joueuse afin d'avoir les informations si tout les membres de l'école d'une joueuse ont été mangés
  * \return rien
 */
-void message_fin_du_jeu(joueuse,joueuse);
+void message_fin_du_jeu(joueuse j1, joueuse j2) {
+    printf("\033[2J"); // Nettoyer le terminal
+    printf("===================================================\n");
+    printf("============ La partie est terminée !! ============\n");
+    printf("===================================================\n");
+    if (tous_manges(j1)) {
+        printf("C'est la joueuse 2 qui gagne ! Il lui reste %d personnages !\n", j2->taille); //TODO
+    } 
+    else {
+        printf("C'est la joueuse 1 qui gagne ! Il lui reste %d personnages !\n", j1->taille); //TODO
+    }
+}
 
 /**
 * \brief Affiche des messages génériques
 * \param int Numéro du message à afficher
 * \param joueuse Joueuse concernéé par le message
-* \param int Index du personnage de la joueuse ou index du numéro de carte utilisé, ... m=0 lorsqu'il n'est pas utile
+* \param int* Index du personnage de la joueuse ou index du numéro de carte utilisé, ... m=0 lorsqu'il n'est pas utile
 */
-void message_generique(int,joueuse,int);
+void message_generique(int n, joueuse j1, int* option) {
+    switch (n) {
+        case 1: fprintf(stderr, "Les personnages ont été correctement initialisés. \n");
+                break;
+        case 2: fprintf(stderr, "Les zones ont été correctement initialisés. \n");
+                break;
+        case 3: printf("La joueuse n° %d a utilisé %d de capital. \n", j1->id, option[0]); //TODO
+                break;
+        case 4: printf("La probabilité de passer de la zone %d à la zone %d a changé de %d. \n", option[0], option[1], option[2]);
+                break;
+        case 5: printf("La joueuse n° %d a utilisé la carte %s", j1->id, getName(lecture_cartes(getMains(j1), option[0]))); //TODO
+                break;
+        case 6: printf("Tous les personnages ont bougé. \n");
+                break;
+        case 7: printf("Le personnage n° %d de la joueuse n° %d a été mangé. \n", option[0], j1->id); //TODO
+                break;
+        case 8: printf("Le tour est terminé, le capital a été réintialisé. \n");
+                break;
+        case 9: fprintf(stderr, "Les personnages ont été correctement libéré par free(). \n");
+                break;
+        case 10: fprintf(stderr, "Les zones ont été correctement libérées par free(). \n");
+                break;
+    }
+}
 
 
