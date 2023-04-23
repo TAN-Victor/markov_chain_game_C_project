fd26f599eb3af0b9908ae21192fe7429279a1c68
Theo taillefumier <theoo.taillefumier@ensiie.fr>
2023-04-23 14:52:10 +0200

    Tâche B2 correction des tests: remplacemement des -> par les getters

--- test.c
+++ test.c
@@ -14,6 +14,7 @@
 #include "headers/zones.h"
 #include "headers/personnages.h"
 #include "headers/cartes.h"
+#include "headers/structures.h"
 
 
 /**
@@ -21,107 +22,106 @@
  * 
  * @param nothing
  * 
- * @return int 1 si initialisée correctement, 0 sinon
+ * @return nothing
  * 
  */
-int test_init_joueuses()
+void test_init_joueuses()
 {
     joueuse j = creation_joueuse(1); /*on créé une joueuse*/
     if (j==NULL){
-        return 0;
+        return;
     }
     if (getCapital(j) != 5){
         fprintf(stderr,"pb capital\n");
-        return 0;
+        return;
     }
     if (getTaille(j) != 5){
         fprintf(stderr,"pb taille\n");
-        return 0;
+        return;
     }
     if (getIdJoueuse(j) != 1){
         fprintf(stderr,"pb id\n");
-        return 0;
+        return;
     }
     if (getTour(j) != 1){
         fprintf(stderr,"pb tour\n");
-        return 0;
+        return;
     }
     if (getToursRestantsBonusCapital(j) != 1){
         fprintf(stderr,"pb tours restants bonus capital\n");
-        return 0;
+        return;
     }
     if (getToursInvincibilite(j) != 0){
         fprintf(stderr,"pb tours invinsibilite\n");
-        return 0;
+        return;
     }
     if (getToursRestantsJouer(j) != 1){
         fprintf(stderr,"pb tours restants jouer\n");
-        return 0;
+        return;
     }
     if (getProbaParCapital(j) != 0.1){
         fprintf(stderr,"pb proba par capital\n");
-        return 0;
+        return;
     }
     if (getMain(j) == NULL){
         fprintf(stderr,"pb mains\n");
-        return 0;
+        return;
     }
     if (getMembres(j) == NULL){
         fprintf(stderr,"pb membres\n");
-        return 0;
+        return;
     }
     //tester la création des personnages
-    free(j);
+    free_joueuse(j);
 
     joueuse m = creation_joueuse(0); /*on créé un monstre*/
     //adapter aux valeurs choisit par adam dans la fonction creation_joueuse
     if (m==NULL){
-        return 0;
+        return;
     }
     if (getCapital(m) != -1){
         fprintf(stderr,"pb capital\n");
-        return 0;
+        return;
     }
     if (getTaille(m) != 1){
         fprintf(stderr,"pb taille\n");
-        return 0;
+        return;
     }
     if (getIdJoueuse(m) != 0){
         fprintf(stderr,"pb id\n");
-        return 0;
+        return;
     }
     if (getTour(m) != 1){
         fprintf(stderr,"pb tour\n");
-        return 0;
+        return;
     }
     if (getToursRestantsBonusCapital(m) != 1){
         fprintf(stderr,"pb tours restants bonus capital\n");
-        return 0;
+        return;
     }
     if (getToursInvincibilite(m) != -1){
         fprintf(stderr,"pb tours invinsibilite\n");
-        return 0;
+        return;
     }
     if (getToursRestantsJouer(m) != 1){
         fprintf(stderr,"pb tours restants jouer\n");
-        return 0;
+        return;
     }
     if (getProbaParCapital(m) != 0.1){
         fprintf(stderr,"pb proba par capital\n");
-        return 0;
+        return;
     }
     if (getMain(m) == NULL){
         fprintf(stderr,"pb mains\n");
-        return 0;
+        return;
     }
     if (getMembres(m) == NULL){
         fprintf(stderr,"pb membres\n");
-        return 0;
+        return;
     }
     //tester la création des personnages
-    free(m);
+    free_joueuse(m);
     fprintf(stdout,"test init joueuses ok\n");
-    return 1;
 }
 
 
@@ -130,34 +130,33 @@ int test_init_joueuses()
  * 
  * @param nothing
  * 
- * @return int 1 si initialisée correctement, 0 sinon
+ * @return nothing
  * 
  */
-int test_init_zones(){
+void test_init_zones(){
     zones z = nouvellesZones(); /*on créé 10 zones*/
     if (z==NULL){
         fprintf(stderr,"NULL pointer");
-        return 0;
+        return;
     }
-    if (getMatrice(z)->taille != 10){
+    if (getTailleMatrice(getMatrice(z)) != 10){
         fprintf(stderr,"pb nb zones\n");
-        return 0;
+        return;
     }
     for (int i=0; i<10; i++){
     if (getTabZones(z)[i] == NULL){
             fprintf(stderr,"pb zone %d\n",i);
-            return 0;
+            return;
         }
-        for (int j=0; j<getMatrice(z)->taille; j++){
+        for (int j=0; j<getTailleMatrice(getMatrice(z)); j++){
             if (lecture_probas(getMatrice(z),i,j) != 0.1){
                 fprintf(stderr,"pb proba %d %d\n",i,j);
-                return 0;
+                return;
             }
             }
         }
     free(z);
     fprintf(stdout,"test init zones ok\n");
-    return 1;
 }
 
 
@@ -166,194 +165,270 @@ int test_init_zones(){
  * 
  * @param nothing
  * 
- * @return int 1 si initialisée correctement, 0 sinon
+ * @return nothing
 */
-int test_init_personnage(){
+void test_init_personnage(){
     zones zs = nouvellesZones();
     personnage perso  = nouveauPersonnage(1,trouveZone(zs,0));//membre de l'équipe 1
     if (perso == NULL){
         fprintf(stderr,"NULL pointer");
-        return 0;
+        return;
     }
     //tester le type:
     if (getType(perso) != 1){
         fprintf(stderr,"pb type joueur 1\n");
-        return 0;
+        return;
     }
     //tester le statut:
     if (getStatut(perso) != 1){
         fprintf(stderr,"pb statut\n");
-        return 0;
+        return;
     }
     //tester id:
     if (getIdPersonnage(perso) != 1){
         fprintf(stderr,"pb id\n");
-        return 0;
+        return;
     }
     //tester la zone courante:
     if (getZoneCourante(perso) != getNumero(trouveZone(zs,0))){
         fprintf(stderr,"pb zone courante\n");
-        return 0;
+        return;
     }
     //tester la zone précédente:
     if (getZonePrecedente(perso) != getNumero(trouveZone(zs,0))){
         fprintf(stderr,"pb zone precedente\n");
-        return 0;
+        return;
     }
     //tester le nombre de pas:
     if (getNbPas(perso) != 1){
         fprintf(stderr,"pb nb pas\n");
-        return 0;
+        return;
     }
     //tester le nombre de tours restants:
     if (getNbTourDisparuRestant(perso) != 0){
         fprintf(stderr,"pb nb tours restants\n");
-        return 0;
+        return;
     }
     //tester si le personnage peut se déplacer:
     if (getPeutSeDeplacer(perso) != 1){
         fprintf(stderr,"pb peut se deplacer\n");
-        return 0;
+        return;
     }
     free(perso);
 
     personnage perso2 = nouveauPersonnage(2,trouveZone(zs,0));//memebre de l'équipe 2
     if (perso2 == NULL){
         fprintf(stderr,"NULL pointer");
-        return 0;
+        return;
     }
     //tester le type:
     if (getType(perso2) != 2){
         fprintf(stderr,"pb type joueur 2\n");
-        return 0;
+        return;
     }
     //tester le statut:
     if (getStatut(perso2) != 1){
         fprintf(stderr,"pb statut\n");
-        return 0;
+        return;
     }
     //tester id:
     if (getIdPersonnage(perso2) != 1){
         fprintf(stderr,"pb id\n");
-        return 0;
+        return;
     }
     //tester la zone courante:
     if (getZoneCourante(perso2) != getNumero(trouveZone(zs,0))){
         fprintf(stderr,"pb zone courante\n");
-        return 0;
+        return;
     }
     //tester la zone précédente:
     if (getZonePrecedente(perso2) != getNumero(trouveZone(zs,0))){
         fprintf(stderr,"pb zone precedente\n");
-        return 0;
+        return;
     }
     //tester le nombre de pas:
     if (getNbPas(perso2) != 1){
         fprintf(stderr,"pb nb pas\n");
-        return 0;
+        return;
     }
     //tester le nombre de tours restants:
     if (getNbTourDisparuRestant(perso2) != 0){
         fprintf(stderr,"pb nb tours restants\n");
-        return 0;
+        return;
     }
     //tester si le personnage peut se déplacer:
     if (getPeutSeDeplacer(perso2) != 1){
         fprintf(stderr,"pb peut se deplacer\n");
-        return 0;
+        return;
     }
     free(perso2);
 
     personnage monstre = nouveauPersonnage(0,trouveZone(zs,0));//monstre
     if (monstre == NULL){
         fprintf(stderr,"NULL pointer");
-        return 0;
+        return;
     }
     //tester le type:
     if (getType(monstre) != 0){
         fprintf(stderr,"pb type monstre\n");
-        return 0;
+        return;
     }
     //tester le statut:
     if (getStatut(monstre) != 1){
         fprintf(stderr,"pb statut\n");
-        return 0;
+        return;
     }
     //tester id:
     if (getIdPersonnage(monstre) != 1){
         fprintf(stderr,"pb id\n");
-        return 0;
+        return;
     }
     //tester la zone courante:
     if (getZoneCourante(monstre) != getNumero(trouveZone(zs,0))){
         fprintf(stderr,"pb zone courante\n");
-        return 0;
+        return;
     }
     //tester la zone précédente:
     if (getZonePrecedente(monstre) != getNumero(trouveZone(zs,0))){
         fprintf(stderr,"pb zone precedente\n");
-        return 0;
+        return;
     }
     //tester le nombre de pas:
     if (getNbPas(monstre) != 1){
         fprintf(stderr,"pb nb pas\n");
-        return 0;
+        return;
     }
     //tester le nombre de tours restants:
     if (getNbTourDisparuRestant(monstre) != 0){
         fprintf(stderr,"pb nb tours restants\n");
-        return 0;
+        return;
     }
     //tester si le personnage peut se déplacer:
     if (getPeutSeDeplacer(monstre) != 1){
         fprintf(stderr,"pb peut se deplacer\n");
-        return 0;
+        return;
     }
     free(monstre);
+    free(zs);
     fprintf(stdout,"test init personnage ok\n");
-    return 1;
 }
 
-
-int test_vie_d_un_personnage(){
+/**
+ * @brief test les déplcements d'un personnage et les modifications de satut ainsi que si le personnage est mangé ou non
+ * 
+ * @param nothing
+ * 
+ * @return nothing
+*/
+void test_vie_d_un_personnage(){
+    zones zs = nouvellesZones();
     personnage perso = nouveauPersonnage(1,trouveZone(zs,0));
     if (getPeutSeDeplacer(perso)){ //on test si le personnage peut se déplacer sur chacune des zones
-        for (int i, i<10, i++){
-            deplacer(perso,i);
+        for (int i=0; i<10; i++){
+            deplacer(perso,trouveZone(zs,i));
+            if (getZoneCourante(perso) != i){
+                fprintf(stderr,"probleme de deplacement\n");
+                return;
+            }
         }
     }
     setStatut(perso,0);
     if (getStatut(perso) != 0){
         fprintf(stderr,"probleme de mis a jour du statut à 0\n");
+        return;
     }
     setStatut(perso,1); 
     if (getStatut(perso) != 1){
         fprintf(stderr,"probleme de mis a jour du statut à 1\n");
+        return;
     }
     setStatut(perso,-1);
     if (getStatut(perso) != -1){
         fprintf(stderr,"probleme de mis a jour du statut à -1\n");
+        return;
     }
     setStatut(perso,3);
     if (getStatut(perso) != 3){
         fprintf(stderr,"probleme de mis a jour du statut à 3\n");
+        return;
     }
-    estMange(personnage perso);
+    estMange(perso);
     if (getStatut(perso) != 0){
         fprintf(stderr,"probleme de mis a jour du statut lorsque le personnage est mangé\n");
+        return;
     }
     free(perso);
+    free(zs);
     fprintf(stdout,"test vie d'un personnage ok\n");
-    return 1;
 }
 
-int test_modif_proba_avec_capital(){
-    
+
+/**
+ * @brief test si la modification de la proba avec le capital fonctionne
+ * 
+ * @param nothing
+ * 
+ * @return nothing
+*/
+void test_modif_proba_avec_capital(){
+    zones zs = nouvellesZones();
+    joueuse j = creation_joueuse(1);
+    float coeff = getProbaParCapital(j);
+    for (int i=0; i<10; i++){ //on test si la modif
+        float sum = 0;
+        for (int j=0; j<10; j++){
+            float prob = lecture_probas(getMatrice(zs),i,j);
+            modifierZone(zs,i,j,3,1);
+            if (3*coeff + prob > 1 || 3*coeff + prob != lecture_probas(getMatrice(zs),i,j)){
+                fprintf(stderr,"probleme de modification de la proba avec le capital\n");
+                return;
+            }
+            sum += lecture_probas(getMatrice(zs),i,j);
+        }
+        if (sum != 1){
+            fprintf(stderr,"probleme de somme des probas\n");
+            return;
+        }
+    }
+    free_joueuse(j);
+    free(zs);
+    fprintf(stdout,"test modif proba avec capital ok\n");
+}
+
+
+/**
+ * @brief test le déroulement du jeu (vainqueur, fin du jeu, démarage du jeu)
+ * 
+ * @param nothing
+ * 
+ * @return nothing
+*/
+void test_deroulement_du_jeu(){
+    joueuse j1 = creation_joueuse(1);
+    zones zs = nouvellesZones();
+    personnage* persos1 = getMembres(j1);
+    int i = 0;
+    while (persos1[i] != NULL){
+        estMange(persos1[i]);
+        i++;
+    }
+    if (!tous_manges(j1)){
+        fprintf(stderr,"probleme de fonction fin de partie\n");
+        return;
+    }
+}
+
+
+void test_affichage(){
+
 }
 
 int main(){
-    int test_zones = test_init_zones();
-    int test_joueuses = test_init_joueuses();
-    int test_personnage = test_init_personnage();
+    test_init_zones();
+    test_init_joueuses();
+    test_init_personnage();
+    test_vie_d_un_personnage();
+    test_modif_proba_avec_capital();
+
     return 0;
 }
