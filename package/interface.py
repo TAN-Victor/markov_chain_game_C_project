8522a2bb58a6a6bb77c27ba8bf8536b255da097a
Victor TAN <victor.tan@gmail.com>
2023-05-29 17:58:42 +0200

    Tache C1 - Déplacement des initialisatinos des objets pour pouvoir les utiliser dans effets.py

--- package/interface.py
+++ package/interface.py
@@ -90,6 +90,84 @@ def probabilites():
     pygame.draw.rect(fenetre, couleur_fond, (longueur*50/100 + 20, largeur*80/100 + 20, longueur*20/100 -10, largeur*20/100 - 30))
     texte = pygame.font.SysFont("Source Sans Pro", 30).render("Voir les probabilités", True, (0, 0, 0))
     fenetre.blit(texte, (longueur*54/100 + 30, largeur*86/100 + 30))
+
+
+##================================================================================================
+# Initialisation des objets
+##================================================================================================
+
+map_objets = {}
+map_boutons = {}
+Zone_joueuse1 = pygame.Rect((longueur*70/100 + 30, largeur*70/100 + 40), (longueur*10/100 - 20, largeur*5/100 - 10))
+Zone_joueuse2 = pygame.Rect((longueur*80/100 + 30, largeur*70/100 + 40), (longueur*10/100 - 20, largeur*5/100 - 10))
+Zone_Zones = pygame.Rect((longueur*50/100 + 30, largeur*80/100 + 40), (longueur*20/100 - 20, largeur*20/100 - 10))
+liste_compteur = [0]*10
+
+def initialiser_objets(liste_joueuses, liste_zones):
+    for i in range (0, 3):
+        for j in range(0, 6):
+            map_objets["personnage_" + str(i+1) + "_" + str(j+1) + "liste"] = (ObjetPersonnageListe(longueur*(70 + i * 10)/100 + 25, 65 + j * 50, 50, "interface/images/" + str(i) + "_" + str(j+1) + ".png", [j+1, "Joueuse n°" + str(i+1)]))
+            map_objets["statut_" + str(i+1) + "_" + str(j+1) + "liste"] = (ObjetStatut(longueur*(70 + i * 10)/100 + 85, 75 + j * 50, 100, "interface/images/" + "En vie" + "_" + str(i) + ".png", [j+1, "Joueuse n°" + str(i+1), "En vie"]))
+            if (j == 5 or (i == 2 and j != 0)):
+                map_objets["personnage_" + str(i+1) + "_" + str(j+1) + "liste"].cacher()
+                map_objets["statut_" + str(i+1) + "_" + str(j+1) + "liste"].cacher()
+    map_objets["tour"] = (ObjetFixe(longueur*(70)/100 + 25, 10, longueur*10/100 -20, None))
+    
+    for i in range(0, 2):
+        for j in range(0, 5):
+            map_objets["carte_" + str(i+1) + "_" + str(j+1)] = ObjetCarte(longueur*(70 + i * 10)/100 + 25, 430 + j * 50, -30, "interface/images/" + liste_joueuses[i].liste_cartes[j].getNom() + "_" + str(i) + ".png", [j+1, i+1])
+    
+    
+    map_boutons["bouton_choix_capital"] = BoutonChoix((longueur*70/100 + 30, largeur*83/100 + 40), (longueur*10/100 - 20, largeur*10/100 - 10), "Utiliser capital", (140, 215, 140), (0, 0, 0))
+    map_boutons["bouton_choix_carte"] = BoutonChoix((longueur*80/100 + 30, largeur*83/100 + 40), (longueur*10/100 - 20, largeur*10/100 - 10), "Utiliser carte", (140, 215, 140), (0, 0, 0))
+    map_boutons["bouton_choix_rien"] = BoutonChoix((longueur*90/100 + 30, largeur*83/100 + 40), (longueur*8/100 - 10, largeur*10/100 - 10), "Ne rien faire", (227, 140, 140), (0, 0, 0))
+
+    map_boutons["bouton_plus"] = BoutonValeur((longueur*90/100 + 30, largeur*82/100 + 40), (longueur*8/100 - 20, largeur*6/100 - 10), "+", (140, 140, 215), (0, 0, 0))
+    map_boutons["bouton_moins"] = BoutonValeur((longueur*72/100 + 30, largeur*82/100 + 40), (longueur*8/100 - 20, largeur*6/100 - 10), "-", (140, 140, 215), (0, 0, 0))
+    map_boutons["bouton_valeur"] = BoutonValeur((longueur*80/100 + 30, largeur*82/100 + 40), (longueur*10/100 - 20, largeur*6/100 - 10), BoutonValeur.valeur, (140, 140, 215), (0, 0, 0))
+    map_boutons["bouton_valeur_valider"] = BoutonValeur((longueur*70/100 + 30, largeur*92/100 + 20), (longueur*15/100 - 50, largeur*8/100 - 30), "Valider", (50, 227, 50), (0, 0, 0))
+    map_boutons["bouton_valeur_annuler"] = BoutonValeur((longueur*85/100 + 30, largeur*92/100 + 20), (longueur*15/100 - 50, largeur*8/100 - 30), "Annuler", (227, 50, 50), (0, 0, 0))
+
+    map_boutons["bouton_carte_valider"] = BoutonValiderCarte((longueur*70/100 + 30, largeur*92/100 + 20), (longueur*15/100 - 50, largeur*8/100 - 30), "Valider", (50, 227, 50), (0, 0, 0))
+    map_boutons["bouton_carte_annuler"] = BoutonValeur((longueur*85/100 + 30, largeur*92/100 + 20), (longueur*15/100 - 50, largeur*8/100 - 30), "Annuler", (227, 50, 50), (0, 0, 0))
+
+    couleurs_zones = [(230, 138, 143), (180, 205, 147), (163, 181, 204), (237, 210, 136), (190, 149, 170), (177, 214, 206), (217, 183, 139), (199, 203, 178), (210, 178, 200), (174, 207, 169), (216, 170, 185)]
+    for i in range(0, 3):
+        for j in range(0, 3):
+            map_boutons["zone_" + str(i+3*j+1)] = BoutonZone((40 + i * 420, 40 +j *200), (400, 180), "Zone n°" + str(i+3*j+1), couleurs_zones[i+3*j], (0, 0, 0), "haut_centre", 0, True)
+    for i in range(0, 2):
+        map_boutons["zone_" + str(i+10)] = BoutonZone((250 + i * 420, 640), (400, 180), "Zone n°" + str(i+10), couleurs_zones[i+9], (0, 0, 0), "haut_centre", 0, True)
+
+
+    
+    demander_action(map_boutons, map_objets, liste_joueuses[0])
+
+    nb_zones = liste_zones.getMatrice().getTailleMatrice()
+    for i in range(nb_zones):
+        for nb in range(0, 3):
+            for j in range(liste_joueuses[nb].taille):
+                if liste_joueuses[nb].getMembres()[j].zone_courante == i:
+                    map_objets["personnage_" + str(nb) + "_" + str(j+1)] = ObjetPersonnage(40 + i%3 * 420 + liste_compteur[i] * 60, 60 + i//3 * 200, 75, "interface/images/" + str(nb) + "_" + str(j+1) + ".png", [j+1, "Joueuse n°" + str(nb + 1)], i, liste_compteur[i])
+                    liste_compteur[i] += 1
+
+    
+    map_boutons["bouton_fin"] = BoutonFin((longueur*35/100 + 30, largeur*70/100 + 40), (longueur*10/100 - 10, largeur*10/100 - 10), "Fin", (227, 140, 140), (0, 0, 0))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ##==============================================================================================
 # UI Dynamique
 ##==============================================================================================
